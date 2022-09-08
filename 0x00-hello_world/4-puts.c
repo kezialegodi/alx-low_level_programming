4780d415797350877e378100007b8489bbe472f8
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-08 09:48:39 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-standard input ouput
 /**
- * main - entry point
- * Return: 0 after printing the function 
+ * main - Entry point
+ *Description: prints "\"Programming is like building a multilingual puzzel"
+ * Return: Always 0 (Success)
  */
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle");
-       return (0);
-}       
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}	       
