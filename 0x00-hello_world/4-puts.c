152c4086a70421f012955c52bcb551c77db9a0d9
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-08 06:22:20 -0700

    task4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
+standard input output
 #include <stdio.h>
 /**
  * main - entry point
  * Return: 0 after printing the function 
  */
 int main(void)
-{./put.c ("\"Programming is like building a multilingual puzzle");
+{puts ("\"Programming is like building a multilingual puzzle");
        return (0);
 }       
