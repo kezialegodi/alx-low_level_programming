0d586d90b257f92b051325a89903e94c820c449a
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-08 05:06:14 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: 0 after printing the function 
  */
 int main(void)
-{put s ("\"Programming is like building a multilingual puzzle");
+{./put.c ("\"Programming is like building a multilingual puzzle");
        return (0);
 }       
