3f82183a758b04ddd9b2e233605024bfd5dc86bf
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-09 11:06:51 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - print if the number is positive, zero, negative
  *
- * Return: Always 0 (Success/correct)
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, negative"
+ * Return: 0
  */
 int main(void)
 
 {
-char alphabet;
+char ch;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-putchar(alphabet);
+	putchar(ch);
 }
 
 putchar('\n');
