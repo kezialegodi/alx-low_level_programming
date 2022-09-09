abce6cc9314e027aea6fa2cce8d750f28bd29fe5
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-09 10:52:03 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,17 +20,17 @@ l = n % 10;
 
 if (l > 5)
 {
-	printf("Last digit %i is %i and is greater than 5\n" , n, l);
+	printf("Last digit of %i is %i and is greater than 5\n" , n, l);
 }
 
 else if (l == 0)
 {
-	printf("Last digit %i is %i and is 0\n", n, l);
+	printf("Last digit of %i is %i and is 0\n", n, l);
 }
 
 else
 {
-	printf("Last digit %i is %i and is less than 6 and not 0\n" , n, l);
+	printf("Last digit of %i is %i and is less than 6 and not 0\n" , n, l);
 }
 
 return (0);
