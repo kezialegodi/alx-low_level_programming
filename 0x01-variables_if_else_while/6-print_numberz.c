d59e5a25f9c294d73eaaddffdd304b8304ea9025
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-09 19:43:22 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print this program if the nnumber is positive, zero, or negative
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative"
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+	putchar (i);
+}
+putchar('\n');
+return (0);
+}
