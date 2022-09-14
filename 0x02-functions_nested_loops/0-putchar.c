f83bb11a5c538f51d4fb4cc7ecc0e94e3fea8a6a
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-14 04:39:58 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - print putchar
+ * main - prints putchar
  * Return always 0
  */
 int main(void)
 {
-	putchar(');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
