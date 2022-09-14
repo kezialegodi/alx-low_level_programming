f153d6288c89ad5d9257257c145e36a31056e6a3
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-14 12:42:17 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,10 @@
 #include "main.h"
 /**
- * _salpha - function to check if c is a letter, lowercase or uppercase
- * @c: is the int that will use for the arguement of the function
- * Return 0
+ * _salpha - checks for alphabetical letters
+ * @c: a charter to be checke on
+ * Return: return 0 or 1 depending on condition
  */
 int _salpha(int c)
 {
-if((c >= 'a' && c <= 'z' || c >= 'A' && c<= 'Z'))
-{
-	return (1);
-}
-else
-return (0);
+return ((c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'))
 }
