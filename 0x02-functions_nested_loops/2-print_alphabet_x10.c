e1550618c3743925fb20a1323629ce93a2c92873
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-14 12:33:25 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
  * print_alphabet_x10 -> prints the lowercase alphabets
- * Return: always 0
  */
 void print_alphabet_x10(void)
 {
 int j;
 int x;
-for(x = 0; x < 10; x++)
+for (x = 0; x < 10; x++)
 {
-for(j = 'a'; j <= 'z'; j++)
+	for(j = 'a'; j <= 'z'; j++)
 {
-	putchar(j);
-	putchar(x);
+	_putchar(j);
+	
 }
 }
-putchar('\n');
-return (0);
+_putchar('\n');
 }
 
