e605dd8feaaa94fad96309eee8d56dc8b148cd71
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-22 08:41:46 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index - 1] = ' ' ||
+		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
