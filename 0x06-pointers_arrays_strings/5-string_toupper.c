f9f9a96618f0fa254e8ba0f2a41c7ead19287c97
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-22 09:07:10 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,8 +9,8 @@ char *string_toupper(char *)
 	int i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if (str[i] >= 97 && str[i] <= '122')
+			str[i] = str[i] - 32;
 		i++;
 	}
 	return (str);
