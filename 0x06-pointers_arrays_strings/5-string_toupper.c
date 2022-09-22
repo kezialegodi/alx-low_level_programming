e24137c92f20fd0d98ba06f403fcdc92fb52722b
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-22 09:34:28 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,14 +2,14 @@
 /**
  * string_toupper - changes all lowercase letters uppercase
  * @str: string to be changed
- * Returns: address to the string
+ * Return: address to the string
  */
 char *string_toupper(char *)
 {
 	int i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= '122')
+		if (str[i] >= 97 && str[i] <= 122)
 			str[i] = str[i] - 32;
 		i++;
 	}
