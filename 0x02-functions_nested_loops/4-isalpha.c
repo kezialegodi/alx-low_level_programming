951c8b7aad5cd439c4fac9ac47958a8fd2709a4d
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-23 10:56:42 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * _salpha - checks for alphabetical letters
- * @c: a charter to be checke on
+ * @c: input letter
  * Return: return 0 or 1 depending on condition
  */
 int _salpha(int c)
 {
 	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		return(0);
+		return (1);
+	else
+		return (0);
 }
