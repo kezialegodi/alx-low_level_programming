dcf37ef936460b3384e368986e380e1acdca2a22
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-23 11:12:32 -0700

    t6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of integer
+ * @ab: input letter
+ * Return: Always success
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
