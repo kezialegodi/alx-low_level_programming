7418f102dde1973caea73b853a50314f22927abe
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-27 11:41:44 -0700

    task7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,13 +10,14 @@ void print_diagsums(int *a, int size)
 	int i, j, p, l = 0, r = 0;
 	for (i = 0; i < size; i++)
 	{
-		p =(i * size) + i;
-		l += *(a + p)
+		p = (i * size) + i;
+		l += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
 		p = (j * size) + (size - 1 - j);
-		r += *(a + p)
+		r += *(a + p);
+	}
 	printf("%i, %in", l, r);
 }	
 
