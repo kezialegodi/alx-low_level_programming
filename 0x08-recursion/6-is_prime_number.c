638ca27220f0762770f9a9c161faf4adb55a29a8
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-28 07:54:02 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,17 +11,3 @@ int is_prime_number(int n)
 		return (0);
 	return (is_prime(n, start));
 }
-/**
- ** is_prime_number - returns the 1 if n is prime
- * @n: the number to be checked
- * @start: number to start checking from
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime(int n, int start);
-{
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
-		return (0);
-	return (is_prime(n, start -1));
-}
