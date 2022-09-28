ea854e5fbb7eeaedde776f4f0ced2b34ba64f2b2
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-28 02:39:45 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: int/ length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
