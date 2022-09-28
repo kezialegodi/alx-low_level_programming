c1bdb2f4dd6b448e58ef91c65fd31147b32328af
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-28 01:33:37 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
-	return;
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 }
