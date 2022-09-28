071de77322a21936c2cf0da5a32a2a02b0695d13
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-28 03:38:32 -0700

    task5

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_recursion(int n, int x);
 int is_prime_number(int n);
-int _sqrt_recursion(int n, int start);
+int is_prime_number(int n, int start);
 #endif /*MAIN_H*/
