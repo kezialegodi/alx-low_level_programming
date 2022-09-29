39b61921d9d502a37c177d9e7f74133be99ec87b
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-29 04:47:49 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,10 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
+	return (_sqrt(0, n));
 }
 /**
- * _sqrt_recursion - return the square root of a number
+ * _sqrt - return the square root of a number
  * @n: test number
  * @x: squared number
  * Return: the square root of n
