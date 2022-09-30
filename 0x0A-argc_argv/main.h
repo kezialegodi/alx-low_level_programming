49a8ca1da96f7ab33670f520311e1f9d895933d1
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-09-30 04:06:56 -0700

    task 1

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,4 +2,9 @@
 #define MAIN_H
 int _putchar(char c);
 int check_num(char *str);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int check_num(char *str);
 #endif /*MAIN_H*/
