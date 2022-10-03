fed461914b99eb1e4f775c4df9af3e28065cefee
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-10-03 07:29:00 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
 	len2 = 0;
-	whilen (s2[len2] != '\0')
+	while (s2[len2] != '\0')
 		len2++;
 	size = len1 + len2;
 	nstr = malloc((sizeof(char) * size) + 1);
@@ -28,13 +28,13 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	while (i < len1)
 	{
-		nstr[i] = s1[i]
+		nstr[i] = s1[i];
 			i++;
 	}
 	j = 0;
 	while (i <= size)
 	{
-		nstr[i] = s2[j]
+		nstr[i] = s2[j];
 			i++;
 			j++;
 	}
