971a13af3860877f65ad8c77193d9a04b81e56da
root <keziahlegodi@git config --global user.email keziahlegodi@gmail.com>
2022-10-13 08:42:56 -0700

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,20 +1,20 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 /**
  * sum_them_all - returns the sum all its parameter
- * @n: amount of arguements
- * Return: sum of its parameter
+ * @n: amount of arguements to be summed
+ * Return: sum or 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	int sum = 0;
+	va_list arglist;
+	unsigned int sum = 0, i;
 	if (n == 0)
 		return (0);
-	va_start(valist, n);
+	va_start(arglist, n);
 	for (i = 0; i < n; i++)
-		sum +- va_arg(valist, int);
-	va_end(valist);
+	{
+		sum += va_arg(arglist, int);
+	}
+	va_end(arglist);
 	return (sum);
 }
