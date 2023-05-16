17eed6baf51a46ac1627afd9928d1d10bd9e0109
kezia <110991756+kezialegodi@users.noreply.github.com>
2023-05-16 12:35:17 +0200

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include<stddef.h>
-#include<stdarg.h>
-#include<string.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-/*Structs*/
-/*Prototypes*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -15,6 +11,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-unsigned long int countbits(unsigned long int n);
-int get_endianness(void);
+
 #endif
