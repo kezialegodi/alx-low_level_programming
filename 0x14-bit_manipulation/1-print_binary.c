83f7331a26129199201f572b14ed89345336cb08
kezia <110991756+kezialegodi@users.noreply.github.com>
2023-05-16 12:29:36 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,31 @@
 #include "main.h"
+#include <unistd.h>
+#include <limits.h>
 /**
- * print_binary - function prints binary conversion of a number
- * @n: unsigned long int number input
+ * print_binary - print an unsigned int in binary
+ *
+ * @n:
+ *
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	n = n & 1;
-	_putchar(n + '0');
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
+
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
+
+	if (printbit == 0)
+		write(1, &c, 1);
+
+	while (printbit)
+	{
+		if (printbit & n)
+			c = '1';
+		else
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
+	}
 }
